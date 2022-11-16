9dce1ba2970ebcf7e95a1d5a4372e4230b36cc34
Skiadas Giorgos <skiadas.giorgos04@gmail.com>
2022-11-16 11:53:58 +0200

    Fix batches, add shuffle inputs before each epoch

--- assignment_1.py
+++ assignment_1.py
@@ -5,21 +5,14 @@ from sklearn.preprocessing import OneHotEncoder
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-y_train = y_train[0:1000]
-x_train = x_train[:1000]
 x_train = x_train.reshape(x_train.shape[0], -1)
 
-def batches(x,y,batch_size):
-    sample = len(x)
-    x_new, y_new = zip(*random.sample(list(zip(x,y)),k=batch_size))
-    return x_new, y_new
-
-print(batches(x,y,4))
 
 def min_max_scaling(inputs):
     inputs_max = np.max(inputs)
     inputs_min = np.min(inputs)
-    return np.divide(np.subtract(inputs,inputs_max), (inputs_max-inputs_min))
+    return (np.divide(np.subtract(inputs, inputs_max), (inputs_max - inputs_min)) - 0.5) * 2
+
 
 def one_hot(y, num_of_classes):
     ohs = np.zeros((y.shape[0], num_of_classes))
@@ -38,7 +31,7 @@ class Layer:
         self.output = 0
         self.number_of_inputs = number_of_inputs
         self.number_of_neurons = number_of_neurons
-        self.weights = 0.1*np.random.randn(number_of_inputs, self.number_of_neurons)
+        self.weights = 0.1 * np.random.randn(number_of_inputs, self.number_of_neurons)
         self.bias = np.zeros([1, number_of_neurons])
 
     def calculate_neuron_output(self, inputs):
@@ -46,10 +39,11 @@ class Layer:
         self.output = np.dot(self.inputs, self.weights) + self.bias
         return self.output
 
-    def backward(self,dvalues):
-        self.dweights = np.dot(np.transpose(self.inputs),dvalues)
-        self.dbias = np.sum(dvalues,axis=0, keepdims=True)
-        self.dinputs = np.dot(dvalues,np.transpose(self.weights))
+    def backward(self, dvalues):
+        self.dweights = np.dot(np.transpose(self.inputs), dvalues)
+        self.dbias = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, np.transpose(self.weights))
+
 
 class ReLU:
     def forward(self, inputs):
@@ -57,9 +51,10 @@ class ReLU:
         self.output = np.maximum(0, inputs)
         return self.output
 
-    def backward(self,dvalues):
+    def backward(self, dvalues):
         self.dvalues = dvalues.copy
         self.dvalues = np.sign(self.inputs)
+
 
 class Softmax:
 
@@ -70,12 +65,13 @@ class Softmax:
         self.output = np.divide(exponent, exponent_sum)
         return self.output
 
-    def backward(self,dvalues):
+    def backward(self, dvalues):
         self.dinputs = np.empty_like(dvalues)
-        for index, (single_output, single_dvalues) in enumerate(zip(self.output,dvalues)):
-            single_output = single_output.reshape(-1,1)
-            jacobian = np.diagflat(single_output) - np.dot(single_output,single_output.T)
-            self.dinputs[index] = np.dot(jacobian,single_dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian, single_dvalues)
+
 
 class Loss:
     def categorical_cross_entropy(self, y_predict, y):
@@ -86,25 +82,26 @@ class Loss:
         data_loss = np.mean(log_likelihood)
         return data_loss
 
-    def categorical_cross_entropy_backward(self,dvalues,y):
+    def categorical_cross_entropy_backward(self, dvalues, y):
         samples = len(dvalues)
         labels = len(dvalues[0])
 
-        self.dinputs = -y/dvalues
-        self.dinputs = self.dinputs/samples
+        self.dinputs = -y / dvalues
+        self.dinputs = self.dinputs / samples
+
 
 class Softmax_loss_combination():
     def __init__(self):
         self.activation = Softmax()
         self.loss = Loss()
 
-    def forward(self,inputs,y):
+    def forward(self, inputs, y):
         self.activation.forward(inputs)
         self.ouput = self.activation.output
 
-        return self.loss.categorical_cross_entropy(self.ouput,y)
+        return self.loss.categorical_cross_entropy(self.ouput, y)
 
-    def backward(self,dvalues,y):
+    def backward(self, dvalues, y):
         sample = len(dvalues)
 
         if len(y.shape) == 2:
@@ -112,65 +109,76 @@ class Softmax_loss_combination():
 
         self.dinputs = dvalues.copy()
         self.dinputs[range(sample), y] -= 1
-        self.dinputs = self.dinputs/sample
+        self.dinputs = self.dinputs / sample
+
 
 class SGD_optimization():
-    def __init__(self,learning_rate):
+    def __init__(self, learning_rate):
         self.learning_rate = learning_rate
 
-    def update_parameters(self,layer):
-        layer.weights += self.learning_rate*layer.dweights
-        layer.bias += self.learning_rate*layer.dbias
+    def update_parameters(self, layer):
+        layer.weights += self.learning_rate * layer.dweights
+        layer.bias += self.learning_rate * layer.dbias
 
-def accuracy(y_pred,y):
-    accuracy = np.mean(y_pred==y)
+
+def accuracy(y_pred, y):
+    accuracy = np.mean(y_pred == y)
     return accuracy
+
+
+def shuffle_inputs(x_input, y_input):
+    number_of_indices = np.arange(0, x_input.shape[0], 1)
+    random.shuffle(number_of_indices)
+    x_output = x_input[number_of_indices]
+    y_output = y_input[number_of_indices]
+    return x_output, y_output
+
 
 soft = Softmax()
 reLu = ReLU()
-SGD = SGD_optimization(learning_rate=0.1)
+SGD = SGD_optimization(learning_rate=0.001)
 soft_loss_combo = Softmax_loss_combination()
 first_layer = Layer(784, 60)
-second_layer = Layer(60, 60)
-output_layer = Layer(60, 10)
-X = x_train
+second_layer = Layer(60, 128)
+third_layer = Layer(128, 256)
+output_layer = Layer(256, 10)
+batch_size = 1000
+
+number_of_runs = int(x_train.shape[0] / batch_size)
 
 for epochs in range(100):
-    first_layer_output = first_layer.calculate_neuron_output(X)
-    first_layer_activation = reLu.forward(first_layer_output)
-    second_layer_output = second_layer.calculate_neuron_output(first_layer_activation)
-    second_layer_activation = reLu.forward(second_layer_output)
-    output_layer_output = output_layer.calculate_neuron_output(second_layer_activation)
-    output_layer_activation = soft.forward(output_layer_output)
-    loss = soft_loss_combo.forward(output_layer_activation, y_train)
-    acc = accuracy(output_layer_activation, y_train)
-    print("Epoch:",end=" ")
-    print(epochs,end=" ")
-    print("Loss:",end=" ")
-    print(loss,end=" ")
-    print("Accuracy:",end=" ")
+    x_train, y_train = shuffle_inputs(x_train, y_train)
+    for step in range(number_of_runs):
+        x_train_batch = x_train[step * (batch_size + 1):(step * (batch_size + 1) + batch_size):1, :]
+        y_train_batch = y_train[step * (batch_size + 1):(step * (batch_size + 1) + batch_size), :]
+        first_layer_output = first_layer.calculate_neuron_output(x_train_batch)
+        first_layer_activation = reLu.forward(first_layer_output)
+        second_layer_output = second_layer.calculate_neuron_output(first_layer_activation)
+        second_layer_activation = reLu.forward(second_layer_output)
+        third_layer_output = third_layer.calculate_neuron_output(second_layer_activation)
+        third_layer_activation = reLu.forward(third_layer_output)
+        output_layer_output = output_layer.calculate_neuron_output(third_layer_activation)
+        output_layer_activation = soft.forward(output_layer_output)
+        loss = soft_loss_combo.forward(output_layer_activation, y_train_batch)
+        acc = accuracy(output_layer_activation, y_train_batch)
+
+        ## Backward pass
+        soft_loss_combo.backward(soft_loss_combo.ouput, y_train_batch)
+        output_layer.backward(soft_loss_combo.dinputs)
+        third_layer.backward(output_layer.dinputs)
+        second_layer.backward(third_layer.dinputs)
+        first_layer.backward(second_layer.dinputs)
+
+        ##Optimization
+
+        SGD.update_parameters(output_layer)
+        SGD.update_parameters(second_layer)
+        SGD.update_parameters(first_layer)
+        SGD.update_parameters(third_layer)
+
+    print("Epoch:", end=" ")
+    print(epochs, end=" ")
+    print("Loss:", end=" ")
+    print(loss, end=" ")
+    print("Accuracy:", end=" ")
     print(acc)
-    ## Backward pass
-    soft_loss_combo.backward(soft_loss_combo.ouput, y_train)
-    output_layer.backward(soft_loss_combo.dinputs)
-    second_layer.backward(output_layer.dinputs)
-    first_layer.backward(second_layer.dinputs)
-
-    ##Optimization
-
-    SGD.update_parameters(output_layer)
-    SGD.update_parameters(second_layer)
-    SGD.update_parameters(first_layer)
-
-    print(first_layer.weights)
-    print(first_layer.bias)
-
-
-
-# soft_output = np.array([[0.7,0.1,0.2],[0.1,0.5,0.4],[0.02,0.9,0.08]])
-# targ = np.array([0,1,1])
-#
-# soft_loss = softmax_loss_combination()
-# soft_loss.backward(soft_output,targ)
-# dvalues1 = soft_loss.dinputs
-# print(dvalues1)
