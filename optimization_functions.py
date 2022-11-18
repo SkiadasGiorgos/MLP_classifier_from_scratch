62418ef856a0557b7ff5ed05853ab80b63dbcbd1
Skiadas Giorgos <skiadas.giorgos04@gmail.com>
2022-11-18 14:48:56 +0200

    RMSProp correction

--- optimization_functions.py
+++ optimization_functions.py
@@ -67,10 +67,12 @@ class RMSProp_optimizer():
         self.learning_rate = self.initial_learning_rate*(1/(1+self.decay_rate*self.iterations))
 
     def update_parameters(self, layer):
-        self.update_learning_rate()
-        layer.weight_cache = self.rho + layer.weight_cache + (1 - self.rho) * layer.dweights ** 2
-        layer.bias_cache = self.rho + layer.bias_cache + (1 - self.rho) * layer.dbias ** 2
+        if self.decay:
+            self.update_learning_rate()
+        layer.weight_cache = self.rho * layer.weight_cache + (1 - self.rho) * layer.dweights ** 2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.dbias ** 2
 
-        layer.weights += self.learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
-        layer.bias += self.learning_rate * layer.dbias / (np.sqrt(layer.bias_cache) + self.epsilon)
-        self.iterations += 1
+        layer.weights += -self.learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.bias += -self.learning_rate * layer.dbias / (np.sqrt(layer.bias_cache) + self.epsilon)
+        if self.decay:
+            self.iterations += 1
